ca392d1d398b856720d8eab65062d6d24d45e894
Themaister <maister@archlinux.us>
2013-04-27 11:24:00 +0200

    Fix broken garbage regression.
    Commit: 1b1336e55f296bafae9676b7a6ebc4512ae3167b.
    S9xGraphicsInit was run before InitROM.
    This caused allocated screen buffer to be half the required size.
    Set SupportHiRes in S9xGraphicsInit instead.

--- src/memmap.c
+++ src/memmap.c
@@ -2096,8 +2096,6 @@ void InitROM (void)
 	Settings.BS = FALSE;
 	SuperFX.nRomBanks = Memory.CalculatedSize >> 15;
    
-   Settings.SupportHiRes = TRUE;
-
 	/* Parse ROM header and read ROM information */
 
 	Memory.CompanyId = -1;
