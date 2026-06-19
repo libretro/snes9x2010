206d40cf4915e36a8ad7cdd688038ad06c2655aa
lcdyk <lcdyk0517@qq.com>
2026-06-20 01:51:10 +0800

    feat: add Simplified and Traditional Chinese translation
    - Add complete Simplified Chinese core option translations
    - Add complete Traditional Chinese core option translations
    - Include Chinese names and descriptions for all options
    - Add Chinese category translations

--- libretro/libretro_core_options.h
+++ libretro/libretro_core_options.h
@@ -313,8 +313,8 @@ struct retro_core_options_v2 *options_intl[RETRO_LANGUAGE_LAST] = {
    NULL,           /* RETRO_LANGUAGE_PORTUGUESE_PORTUGAL */
    NULL,           /* RETRO_LANGUAGE_RUSSIAN */
    NULL,           /* RETRO_LANGUAGE_KOREAN */
-   NULL,           /* RETRO_LANGUAGE_CHINESE_TRADITIONAL */
-   NULL,           /* RETRO_LANGUAGE_CHINESE_SIMPLIFIED */
+   &options_cht,   /* RETRO_LANGUAGE_CHINESE_TRADITIONAL */
+   &options_chs,   /* RETRO_LANGUAGE_CHINESE_SIMPLIFIED */
    NULL,           /* RETRO_LANGUAGE_ESPERANTO */
    NULL,           /* RETRO_LANGUAGE_POLISH */
    NULL,           /* RETRO_LANGUAGE_VIETNAMESE */
